abae3a372929cdaef925fe364bc35a7d1915656c
Camilo Reyes Granada <camilo.reyes_granada@uao.edu.co>
2024-10-26 10:00:48 -0500

    add: game logic to options

--- src/components/dialog/Options.tsx
+++ src/components/dialog/Options.tsx
@@ -1,21 +1,25 @@
-interface Option {
-  id: number;
-  text: string;
-  action: () => void;
-}
+import { GameOptionI } from '../../interfaces/gameOption.interface';
+
 
 interface OptionsProps {
-  options: Option[];
+  options: GameOptionI[];
+  onSelectOption: (option: GameOptionI) => void;
 }
 
-export const Options = ({ options }: OptionsProps) => {
+export const Options = ({ options, onSelectOption }: OptionsProps) => {
+  const handleOnClick = (option: GameOptionI) => {
+
+
+    onSelectOption(option); // Llama `onSelectOption` con el tipo `GameOptionI`
+  };
+
   return (
-    <div className="flex flex-col p-2 bg-slate-200 max-w-6xl m-auto rounded-xl mb-10 ">
+    <div className='flex flex-col p-2 bg-slate-200 max-w-6xl m-auto rounded-xl mb-10 '>
       {options.map((option) => (
         <button
           key={option.id}
-          className="p-2 hover:bg-slate-400 bg-slate-300 rounded-md m-1"
-          onClick={option.action}
+          className='p-2 hover:bg-slate-400 bg-slate-300 rounded-md m-1'
+          onClick={() => handleOnClick(option)}
         >
           {option.text}
         </button>
