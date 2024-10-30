a0bff34071d46884c3a145d5d0a21f91b81ece34
Camilo Reyes Granada <camilo.reyes_granada@uao.edu.co>
2024-10-29 23:05:42 -0500

    add: tailwind animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-in-left': {
+          '0%': { opacity: '0', transform: 'translateX(-50%)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        'fade-down': {
+          '0%': { opacity: '1', transform: 'translateY(0)' },
+          '100%': { opacity: '1', transform: 'translateY(-100%)' },
+        },
+      },
+      animation: {
+        'fade-in-left': 'fade-in-left 1.2s ease-in forwards',
+        'fade-down': 'fade-down 1s ease-in forwards',
+      },
+    },
   },
-  plugins: [],
+    plugins: [
+    require('tailwindcss-animated')
+  ],
 }
 
