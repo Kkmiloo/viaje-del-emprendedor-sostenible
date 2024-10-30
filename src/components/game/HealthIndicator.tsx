66b39bb009f3c78c1f1a349575c5fe4ab7d73375
Camilo Reyes Granada <camilo.reyes_granada@uao.edu.co>
2024-10-30 00:40:05 -0500

    add: health indicator component

--- src/components/game/HealthIndicator.tsx
+++ src/components/game/HealthIndicator.tsx
@@ -1,8 +1,19 @@
 import { useGameStore } from "../../store"
 
 export const HealthIndicator = () => {
-    const { lives } = useGameStore();
-    const totalLives = 3 -lives
-    
-    return <div>{'🧡'.repeat(lives) + '🖤'.repeat(totalLives)}</div>;
-}
+  const { lives } = useGameStore();
+  const totalLives = 3;
+
+  return (
+    <div className='bg-gray-300 flex items-center px-5 md:px-16 rounded-md h-10 space-x-1'>
+      {[...Array(totalLives)].map((_, index) => (
+        <span
+          key={index}
+          className={index < lives ? 'text-orange-500' : 'text-gray-500'}
+        >
+          {index < lives ? '🧡' : '🖤'}
+        </span>
+      ))}
+    </div>
+  );
+};
