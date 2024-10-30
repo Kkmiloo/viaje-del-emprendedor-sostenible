2b0a9dfd99e1ae4b6a4e971e9e861392937907ed
Camilo Reyes Granada <camilo.reyes_granada@uao.edu.co>
2024-10-29 23:11:31 -0500

    add: style

--- src/layout/GameLayout.tsx
+++ src/layout/GameLayout.tsx
@@ -2,8 +2,8 @@ import { Outlet } from 'react-router-dom';
 
 export const GameLayout = () => {
   return (
-    <div className='min-w-full min-h-screen bg-slate-400 flex flex-col'>   
-          <Outlet />
-      </div>
+    <div className='min-w-full min-h-screen game-bg-dots flex flex-col figtree'>
+      <Outlet />
+    </div>
   );
 }
