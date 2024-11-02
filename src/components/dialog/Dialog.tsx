ddca0c37fc74f6efdedc69c58e6f0f6c216deceb
Camilo Reyes Granada <camilo.reyes_granada@uao.edu.co>
2024-11-01 22:53:35 -0500

    add:  dialog and question

--- src/components/dialog/Dialog.tsx
+++ src/components/dialog/Dialog.tsx
@@ -1,13 +1,18 @@
 import { useState, useRef, useEffect } from 'react';
+import man from '../../assets/man.webp';
+import { TypeAnimation } from 'react-type-animation';
 
 interface DialogProps {
   text: string;
+  question?: string;
+  onNext: () => void;
 }
 
-export const Dialog = ({ text }: DialogProps) => {
+export const Dialog = ({ text, question, onNext }: DialogProps) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [currentPage, setCurrentPage] = useState(0);
   const [pages, setPages] = useState<string[]>([]);
+  const [showQuestion, setShowQuestion] = useState(false);
 
   // Función para dividir el texto en partes según el tamaño del contenedor
   const paginateText = (text: string, charsPerPage: number) => {
@@ -33,6 +38,8 @@ export const Dialog = ({ text }: DialogProps) => {
     return result;
   };
 
+  console.log(pages.length);
+
   // Efecto para calcular el tamaño del contenedor y paginar el texto
   useEffect(() => {
     if (containerRef.current) {
@@ -54,21 +61,59 @@ export const Dialog = ({ text }: DialogProps) => {
   const handleNext = () => {
     if (currentPage < pages.length - 1) {
       setCurrentPage(currentPage + 1);
+    } else if (question) {
+      setShowQuestion(true);
+      onNext(); // Ejecuta la función al terminar la última página y tener pregunta
     }
   };
 
   return (
-    <div className='py-10 px-10 md:py-10 text-gray-800 text-xl font-medium rounded-xl border-4 bg-gray-100 max-h-56 w-full md:max-h-64 h-[300px] max-w-6xl m-auto z-20'>
-      <div ref={containerRef} className='h-full overflow-auto'>
-        <p>{pages[currentPage]}</p>
-        {currentPage < pages.length - 1 && (
-          <button
-            className='absolute bottom-2 right-2 bg-white text-yellow-900 px-4 py-2 rounded'
-            onClick={handleNext}
-          >
-            Siguiente
-          </button>
-        )}
+    <div
+      className={`${
+        !showQuestion ? 'cursor-pointer' : ''
+      }  py-10 px-6 md:py-10 text-gray-800 text-xl font-medium rounded-xl border-4 bg-gray-100 max-h-56 w-full md:max-h-64 h-[300px] max-w-6xl m-auto z-20`}
+      onClick={handleNext}
+    >
+      <div
+        ref={containerRef}
+        className='h-full overflow-auto flex flex-col justify-between'
+      >
+        <div className='flex h-full items-center'>
+          <img
+            src={man}
+            className='max-w-32 w-fit h-fit rounded-xl border-4 border-slate-600 bg-red-500 p-2'
+          />
+          <div className='flex flex-col h-full justify-between ml-8 w-full'>
+            {currentPage <= pages.length - 1 && !showQuestion && (
+              <div className={` mt-4 `}>
+                <TypeAnimation sequence={[pages[currentPage]]} speed={50} />
+              </div>
+            )}
+            {showQuestion && question && (
+              <div className='mt-4 text-center'>
+                <TypeAnimation sequence={[question]} speed={50} repeat={1}/>
+              </div>
+            )}
+
+            { !showQuestion && (
+              <div className='animate-bounce w-full'>
+                <svg
+                  className='m-auto '
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='30'
+                  height='30'
+                  viewBox='0 0 512 298.04'
+                  fill='#334155'
+                >
+                  <path
+                    fillRule='nonzero'
+                    d='M12.08 70.78c-16.17-16.24-16.09-42.54.15-58.7 16.25-16.17 42.54-16.09 58.71.15L256 197.76 441.06 12.23c16.17-16.24 42.46-16.32 58.71-.15 16.24 16.16 16.32 42.46.15 58.7L285.27 285.96c-16.24 16.17-42.54 16.09-58.7-.15L12.08 70.78z'
+                  />
+                </svg>
+              </div>)
+            }
+          </div>
+        </div>
       </div>
     </div>
   );
