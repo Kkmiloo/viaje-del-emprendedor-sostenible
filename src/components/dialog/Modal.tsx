5d7d3009565f30e1106c7658358c1ff2ea907891
Camilo Reyes Granada <camilo.reyes_granada@uao.edu.co>
2024-11-04 11:36:41 -0500

    add: modal children

--- src/components/dialog/Modal.tsx
+++ src/components/dialog/Modal.tsx
@@ -1,17 +1,24 @@
 
+import { ReactNode } from "react";
+
 interface ModalProps{
     text: string;
-    onConfirm: () => void;
+  onConfirm: () => void;
+  children?: ReactNode
 }
 
-export const Modal = ({text, onConfirm}:ModalProps) => {
+export const Modal = ({text, onConfirm, children}:ModalProps) => {
   return (
-    <div onClick={onConfirm} className="cursor-pointer">
+    <div
+      onClick={onConfirm}
+      className='cursor-pointer '
+    >
       <div className='fixed inset-0 flex items-center justify-center z-50'>
         <div className='modal-overlay absolute inset-0 bg-gray-500 opacity-50'></div>
         <div className='modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto'>
           <div className='modal-content py-4 text-left px-6 min-h-40'>
             <p className='text-black'>{text}</p>
+            <div className='mt-4'>{children}</div>
           </div>
         </div>
       </div>
