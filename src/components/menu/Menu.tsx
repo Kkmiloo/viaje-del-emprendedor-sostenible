af837fdd359fafedae459b600f8258446864afc5
Camilo Reyes Granada <camilo.reyes_granada@uao.edu.co>
2024-11-07 13:18:35 -0500

    save: pc problem

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -2,20 +2,29 @@ import { Link } from "react-router-dom";
 
 export const Menu = () => {
   return (
-    <nav className='bg-gray-800 text-white p-4 h-full '>
-      <ul className='flex flex-col max-w-6xl m-auto'>
-        <li>
-          <Link to='./jugar' className='hover:text-yellow-400'>
+    <nav className='  p-4 h-full text-white '>
+      <ul className='flex flex-col max-w-6xl m-auto items-center gap-3'>
+        <li className='w-80 '>
+          <Link
+            to='./jugar'
+            className='shadow-sm shadow-white bg-slate-600 hover:bg-slate-700 w-full p-2 rounded-lg flex items-center justify-center '
+          >
             Jugar
           </Link>
         </li>
-        <li>
-          <Link to='./tutorial' className='hover:text-yellow-400'>
+        <li className='w-80'>
+          <Link
+            to='./tutorial'
+            className='border w-full p-2 rounded-lg flex items-center justify-center '
+          >
             Tutorial
           </Link>
         </li>
-        <li>
-          <Link to='./opciones' className='hover:text-yellow-400'>
+        <li className='w-80'>
+          <Link
+            to='./opciones'
+            className='border w-full p-2 rounded-lg flex items-center justify-center '
+          >
             Opciones
           </Link>
         </li>
--- src/layout/MenuLayout.tsx
+++ src/layout/MenuLayout.tsx
@@ -3,17 +3,18 @@ import { Menu } from '../components/menu/Menu';
 
 export const MenuLayout = () => {
   return (
-    <div className='flex flex-col h-screen'>
-      <header className='bg-yellow-900 p-4'>
-        <h1 className='text-white text-3xl'>Energy Empire: El Ascenso Solar</h1>
+    <div className='flex flex-col h-screen chewy text-center bg-gradient-to-r from-slate-900 to-slate-700 pt-32'>
+      <header className='p-4'>
+        <h1 className=' text-white text-5xl'>
+          Energy Empire: El Ascenso Solar
+        </h1>
       </header>
 
-      <main className='flex-grow bg-gray-400'>
+      <main className='flex-grow text-2xl'>
         <Menu />
-        {/* Aquí iría el contenido principal del juego */}
       </main>
 
-      <footer className='bg-gray-800 text-white p-4 text-center'>
+      <footer className=' text-white p-4 text-center '>
         <p>&copy; 2024 Energy Empire: El Ascenso Solar</p>
       </footer>
     </div>
